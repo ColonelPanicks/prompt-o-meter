ad819c3337e3e2d382ed16107b3d7b885160bc7f
stuts <stu.j.franks@gmail.com>
2020-07-31 22:34:16 +0100

    Add helpful comments to code

--- prompt-o-meter.rb
+++ prompt-o-meter.rb
@@ -1,20 +1,25 @@
 #!/usr/bin/env ruby
 
+# Load YAML support
 require 'YAML'
 
+# Load the YAML from questions.yaml in as a hash
 questions = YAML.load_file('questions.yaml')
+
+# Load the YAML from lists.yaml  in as a hash that's globally accessible
 @lists = YAML.load_file('lists.yaml')
 
+# Define what the {{BLANK}} text should be replaced with, globally accessible
 @blank = "______"
 
-
+# Create a function called ask_questions that takes one argument ("questions") that will be used when the function is run
 def ask_question(questions)
-  # Select one question from the list
+  # Select one question from the list by selecting a random key (sample) from the question hash 
   q = questions[questions.keys.sample]
 
   # Separate question and lists
-  question = q.delete('question')
-  options = q
+  question = q.delete('question') # This pops out the contents of the question key and captures it in a variable called question
+  options = q # The rest of the data left in q is what we'll look for to replace
 
   # Loop through options
   options.each do |option, list|
@@ -29,10 +34,9 @@ def ask_question(questions)
   end
 
   # Ask question
-  puts "ASKING QUESTION"
   puts question
-  puts
 
 end
 
+# Run the function, passing through the questions we loaded in at the beginning
 ask_question(questions)
